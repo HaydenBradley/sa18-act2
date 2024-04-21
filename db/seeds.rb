64a975e80286758f0c0208bbf77129d67918a807
Nicholas Bradley <bradleyhayden6932@gmail.com>
2024-04-21 12:57:46 -0500

    The website is running. It opens from localhost:3000/products. Immediately you see all the products and then you can click them to view them in more depth.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create(name: "Xbox 1", price: 300, description: "Xbox 1 for sale! Great condition.")
+Product.create(name: "Macbook Pro", price: 1000, description: "2023 Macbook Pro brand new. Limited Time Offer")
+Product.create(name: "Iphone 15", price: 600, description: "Iphone 15 with slight damage. Works perfectly")
